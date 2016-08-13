8bcae594a10ccc5f3502707672e3f6428eecae5a
Martin Nadal <martin.nadal@gmail.com>
2016-08-13 18:19:00 +0200

    encoding using audiobuffers

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 var audioCtx = null;
 var gainNode = null;
-var sounds = null;
+
+var buffers = null;
+var sampleRate = null;
 var playingSound = null;
 
 var origHTML;
@@ -12,7 +14,8 @@ $(document).ready(function(){
 
   ditherImages();
   initText($('.loadingText p'));
-  playStartBlock($('.loadingText p'));
+  var text = $('.loadingText').text();
+  playString(text,1.2);
 });
 
 
@@ -20,119 +23,72 @@ function initAudio(){
   // create web audio api context
   audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
+  sampleRate = 22100;
   gainNode = audioCtx.createGain();
   gainNode.gain.value = 0.1;
   gainNode.connect(audioCtx.destination);
 
-  // create Oscillator node
-  var sound0 = audioCtx.createOscillator();
-
-  sound0.type = 'square';
-  sound0.frequency.value = 2044; // value in hertz
-  sound0.start();
-
-  var sound1 = audioCtx.createOscillator();
-
-  sound1.type = 'square';
-  sound1.frequency.value = 1022; // value in hertz
-  sound1.start();
-
-  sounds = {0:{sound:sound0,ms:1000.0/sound0.frequency.value},
-            1:{sound:sound1,ms:1000.0/sound1.frequency.value}};
-
-}
-
-function playStartBlock(domElement){
-
-  //leader block 2ms at 2500
-  // leader tone
-  var leader = audioCtx.createOscillator();
-
-  leader.type = 'square';
-  leader.frequency.value = 808; // value in hertz
-  leader.start();
-  leader.connect(gainNode);
-
-  setTimeout(function(){
-
-    leader.stop();
-    leader.disconnect();
-    //starting block 2ms at 2500
-    var blockStart = audioCtx.createOscillator();
-
-    blockStart.type = 'square';
-    blockStart.frequency.value = 2500; // value in hertz
-    blockStart.start();
-    blockStart.connect(gainNode);
-
-    setTimeout(function(){
-      blockStart.stop();
-      blockStart.disconnect();
-      playText(domElement);
-    },2);
-
-  },1000);
-}
-
-function playText(domElement,charIndex,bitIndex){
-
-  var currentElem = domElement.first();
-
-  if(charIndex === undefined){
-    charIndex = 0;
-    currentElem.find('span').first().addClass('loading');
-  }
-
-
-  var string = currentElem.data('text');
-  //http://stackoverflow.com/a/5409767/2205297
-  bitIndex   = (bitIndex === undefined) ? 7 : bitIndex;
-
-  var bit = (string.charCodeAt(charIndex) >> bitIndex) & 1;
-
-  var timeout = sounds[bit].ms;
-
-  //just disconnect if prev bit was different
-  if(prevbit != bit){
-    if(playingSound != null){
-      playingSound.disconnect();
+  //create buffers for each frequency
+  var bufferFreqs = [808,1022,2044,2500];
+  buffers = {};
+  for (var i=0;i<bufferFreqs.length;i++) {
+    var bufferFreq = bufferFreqs[i];
+    buffers[bufferFreq] = new Array();
+    for(var j = 0;j<1.0/bufferFreq * sampleRate ;j++){
+      var t = j/sampleRate;
+      buffers[bufferFreq].push(Math.sign(Math.sin(2*Math.PI*t*bufferFreq)));
     }
-    playingSound = sounds[bit].sound;
-    playingSound.connect(gainNode);
-  }
-  prevbit = bit;
-
-  bitIndex --;
-  if(bitIndex < 0){
-    bitIndex = 7;
-    charIndex ++;
-
-    //end text
-    if(charIndex >= string.length){
-      playingSound.disconnect();
-      playingSound = null;
-      currentElem.find('span').removeClass('loading').addClass('loaded');
-      if(domElement.size()>1){
-        playStartBlock(domElement.nextAll());
-      }
-      return;
-    }
-
-    currentElem.find('span').each(function(i){
-      var self  = $(this);
-      var start = self.data('start');
-      var end   = self.data('end');
-
-      //@todo:fix when charIndex == 0
-      if(charIndex == start){
-        self.prev().removeClass('loading').addClass('loaded');
-        self.addClass('loading');
-      }
-    })
   }
 
-  setTimeout(function(){playText(domElement,charIndex,bitIndex)},timeout);
 }
+
+function playString(string,leadTime = 0){
+
+  var bits = [];
+  var frameCount = 0;
+  var bitBuffers = [buffers['1022'],buffers['2044']];
+  var leadFrames = 0;
+
+  for (var charIndex = 0;charIndex<string.length;charIndex++) {
+    var charCode = string.charCodeAt(charIndex);
+
+    for(var bitIndex=7;bitIndex>=0;bitIndex--){
+        var bit = (charCode >> bitIndex) & 1;
+        bits.push(bit);
+        frameCount += bitBuffers[bit].length;
+    }
+  }
+
+
+  if (leadTime > 0) {
+    leadFrames = leadTime * sampleRate
+    frameCount += leadFrames; //add one second
+  }
+
+  myArrayBuffer = audioCtx.createBuffer(1, frameCount, sampleRate );
+  var nowBuffering = myArrayBuffer.getChannelData(0);
+  var bufferIndex = 0;
+  var leadBuffer = buffers['808'];
+
+  for(bufferIndex = 0;bufferIndex < leadFrames;bufferIndex++){
+    nowBuffering[bufferIndex] = leadBuffer[bufferIndex % leadBuffer.length];
+  }
+
+  for(var i=0;i<bits.length;i++){
+    var buffer = bitBuffers[bits[i]];
+    for(var j=0;j<buffer.length;j++){
+      nowBuffering[bufferIndex] = buffer[j];
+      bufferIndex ++;
+    }
+  }
+
+  var source = audioCtx.createBufferSource();
+  source.buffer = myArrayBuffer;
+  source.connect(audioCtx.destination);
+  // start the source playing
+  source.start();
+}
+
 
 //insert every word inside a span
 function initText(domElement){
@@ -171,7 +127,8 @@ function initText(domElement){
 }
 
 function playTextArea(){
-
+  playString('pepebjbjbjhjkhkjh');
+  /*
   if(playingSound != null){
     return;
   }
@@ -179,5 +136,5 @@ function playTextArea(){
   domElement = $(selector);
 
   playStartBlock(domElement);
-
+ */
 }
